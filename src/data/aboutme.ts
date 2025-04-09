4fd4912549930d9b70e46c78526f8ea369047e1f
Bassy01 <110795570+Bassy01@users.noreply.github.com>
2025-04-09 16:02:29 +0900

    Update aboutme

--- src/data/aboutme.ts
+++ src/data/aboutme.ts
@@ -18,22 +18,22 @@ export interface AboutMe {
 }
  
 export const aboutMe: AboutMe = {
-  name: "Jane R. Smith",
-  title: "Ph.D. Candidate",
-  institution: "Stanford University",
+  name: "Emi Ichimura",
+  title: "Preparing for Graduate School",
+  institution: "",
   // Note that links work in the description
   description:
-    "I'm a final-year <a href='https://www.stanford.edu'>PhD candidate</a> working at the intersection of causal inference and machine learning. My research focuses on developing robust, interpretable systems that can reason about cause and effect in complex environments.",
-  email: "______@stanford.edu",
-  imageUrl:
-    "https://images.unsplash.com/photo-1581481615985-ba4775734a9b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  googleScholarUrl: "https://scholar.google.com/citations?user=bWtMl_MAAAAJ",
-  githubUsername: "janesmith",
-  linkedinUsername: "janesmith",
-  twitterUsername: "janesmith",
-  blogUrl: "https://",
-  cvUrl: "https://",
-  institutionUrl: "https://www.stanford.edu",
+    "I'm a recent-graduate from <a href='https://www.iniad.org/'> Toyo University </a> and majoring computer science. My research focuses on Human-Computer Interaction (HCI) and  Educational Technology (EdTech).",
+  email: "ichimura.emi01@gmail.com",
+  imageUrl: '/profile_20250401.jpg',
+  // "https://images.unsplash.com/photo-1581481615985-ba4775734a9b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  // googleScholarUrl: "https://scholar.google.com/citations?user=bWtMl_MAAAAJ",
+  githubUsername: "Emi-Bassy",
+  linkedinUsername: "emi-ichimura",
+  // twitterUsername: "janesmith",
+  blogUrl: "https://emi-75.hatenablog.com/",
+  // cvUrl: "https://",
+  // institutionUrl: "https://www.stanford.edu",
   // altName: "",
   // secretDescription: "I like dogs.",
 };
